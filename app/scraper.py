10b3a663f09de493da4db86e1ca81002066929fe
Jessica-ovabor <ovaborjessica85@gmail.com>
2026-01-03 20:29:05 +0100

    (local_llm and scrapper)

--- app/scraper.py
+++ app/scraper.py
@@ -5,6 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
+def driver_client():
+    driver = webdriver.Chrome()
+    return driver
+
+
 def web_scraper(url):
     driver = webdriver.Chrome()
     driver.get(url)
@@ -22,4 +27,22 @@ def web_scraper(url):
         scraped_data.append(result.text)
 
     driver.quit()
+    return scraped_data
+
+
+def dynamic_web_scraper(url):
+    web_driver = driver_client()
+    webdriver.get(url)
+    search = web_driver.find_element(By.CSS_SELECTOR, "InlineSearch")
+
+    results = WebDriverWait(web_driver, 30).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "SearchResultItem"))
+    )
+
+    scraped_data = []
+
+    for result in results[:3]:
+        scraped_data.append(result.text)
+
+    web_driver.quit()
     return scraped_data
